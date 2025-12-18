1214c03f9491a81d9a5b61c018492711e956fe51
abdurrahman482937 <abdurrahmanass2024@gmail.com>
2025-12-18 11:38:42 +0600

    fix: correct user data field name and formatting in syncUserCreation function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -11,12 +11,12 @@ const syncUserCreation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
-      id: id,
+      _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + "" + last_name,
+      name: first_name + " " + last_name,
       image: image_url,
     };
-    await User.create(userData);
+    await User.create(userData);    
   }
 );
 
