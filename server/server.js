64df61dcc683d4285b336a6e5957830e536917eb
abdurrahman482937 <abdurrahmanass2024@gmail.com>
2025-12-19 23:56:01 +0600

    server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,10 +21,12 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 // API Routes
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.get("/", (req, res) => res.send("Hello, World!"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/show", showRouter);
+app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
