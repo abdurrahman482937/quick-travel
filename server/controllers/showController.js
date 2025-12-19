39f49eef3c732371f5a16ac11f3d0c981aff22f7
abdurrahman482937 <abdurrahmanass2024@gmail.com>
2025-12-19 23:55:10 +0600

    controllers

---
+++ server/controllers/showController.js
@@ -0,0 +1,120 @@
+import axios from "axios";
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js";
+
+export const getNowPlayingMovies = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+      }
+    );
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const addShow = async (req, res) => {
+  try {
+    const { movieId, showsInput, showPrice } = req.body;
+    let movie = await Movie.findById(movieId);
+    if (!movie) {
+      // Fetch movie details and credits from TMDB API
+      const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+      ]);
+
+      const movieDetails = {
+        _id: movieId,
+        title: movieDetailsResponse.data.title,
+        overview: movieDetailsResponse.data.overview,
+        poster_path: movieDetailsResponse.data.poster_path,
+        backdrop_path: movieDetailsResponse.data.backdrop_path,
+        genres: movieDetailsResponse.data.genres,
+        casts: movieCreditsResponse.data.cast,
+        release_date: movieDetailsResponse.data.release_date,
+        original_language: movieDetailsResponse.data.original_language,
+        tagline: movieDetailsResponse.data.tagline || "",
+        vote_average: movieDetailsResponse.data.vote_average,
+        runtime: movieDetailsResponse.data.runtime,
+      };
+      // Add movie to the database
+      movie = await Movie.create(movieDetails);
+    }
+
+    const showsToCreate = [];
+    showsInput.forEach((show) => {
+      const showDate = show.date;
+      show.time.forEach((time) => {
+        const dateTimeString = `${showDate}T${time}`;
+        showsToCreate.push({
+          movie: movieId,
+          showDateTime: new Date(dateTimeString),
+          showPrice,
+          occupiedSeats: {},
+        });
+      });
+    });
+
+    if (showsToCreate.length > 0) {
+      await Show.insertMany(showsToCreate);
+    }
+
+    res.json({ success: true, message: "Show added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get all shows from the database
+export const getShows = async (req, res) => {
+  try {
+    const shows = await Show.find({ showDateTime: { $gte: new Date() } })
+      .populate("movie")
+      .sort({ showDateTime: 1 });
+    // filter unique shows
+    const uniqueShows = new Set(shows.map((show) => show.movie));
+    res.json({ success: true, shows: Array.from(uniqueShows) });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get a single show from the database
+export const getShow = async (req, res) => {
+  try {
+    const { movieId } = req.params;
+    // get all upcoming shows for the movie
+    const shows = await Show.find({
+      movie: movieId,
+      showDateTime: { $gte: new Date() },
+    });
+
+    const movie = await Movie.findById(movieId);
+    const dateTime = {};
+
+    shows.forEach((show) => {
+      const date = show.showDateTime.toISOString().split("T")[0];
+      if (!dateTime[date]) {
+        dateTime[date] = [];
+      }
+      dateTime[date].push({ time: show.showDateTime, showId: show._id });
+    });
+    res.json({ success: true, movie, dateTime });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
